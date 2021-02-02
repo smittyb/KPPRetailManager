66316e6af974473896640a5d7dcc5583bd598b1e
smittyb <smittyb.sia@gmail.com>
2021-02-02 14:04:33 -0800

    Lesson 13 completed and working

---
+++ KPPDataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,24 @@
+﻿using KPPDataManager.Library.Internal.DataAccess;
+using KPPDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPPDataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public List<UserModel> GetUserById(string Id)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            var p = new { Id = Id };
+
+            var output = sql.LoadData<UserModel, dynamic>("dbo.spUserLookup", p, "KPPData");
+
+            return output;
+        }
+    }
+}
---
+++ KPPDataManager/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using KPPDataManager.Library.DataAccess;
+using KPPDataManager.Library.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace KPPDataManager.Controllers
+{
+    [Authorize]
+
+    public class UserController : ApiController
+    {
+      
+        public List<UserModel> GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+            UserData data = new UserData();
+
+            return data.GetUserById(userId);
+        }
+    }
+}
