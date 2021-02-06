8e00709b7b8fe66c7ffbd5a1e415d5ee5afde933
smittyb <smittyb.sia@gmail.com>
2021-02-06 11:07:16 -0800

    Corrected 2 errors in "ItemQuantity" from Lesson  15

--- KPPDesktopUI/ViewModels/SalesViewModel.cs
+++ KPPDesktopUI/ViewModels/SalesViewModel.cs
@@ -22,15 +22,15 @@ namespace KPPDesktopUI
             }
         }
 
-        private string _itemQuantity;
+        private int _itemQuantity;
 
-        public string ItemQuantity
+        public int ItemQuantity
         {
             get { return _itemQuantity; }
             set
             { 
                 _itemQuantity = value;
-                NotifyOfPropertyChange(() => Products);
+                NotifyOfPropertyChange(() => ItemQuantity);
             }
         }
 
