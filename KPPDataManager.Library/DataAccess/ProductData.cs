e7aeba5f8638485a495a2eee25c455bc04fa07c0
smittyb <smittyb.sia@gmail.com>
2021-02-06 23:02:50 -0800

    Lesson 17 Completed and Working.

---
+++ KPPDataManager.Library/DataAccess/ProductData.cs
@@ -0,0 +1,23 @@
+﻿using KPPDataManager.Library.Internal.DataAccess;
+using KPPDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPPDataManager.Library.DataAccess
+{
+    public class ProductData
+    {
+        public List<ProductModel> GetProducts()
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+
+            var output = sql.LoadData<ProductModel, dynamic>("dbo.spProduct_GetAll", new { }, "KPPData");
+
+            return output;
+        }
+    }
+}
---
+++ KPPDesktopUI.Library/Api/IProductEndpoint.cs
@@ -0,0 +1,11 @@
+﻿using KPPDesktopUI.Library.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace KPPDesktopUI.Library.Api
+{
+    public interface IProductEndpoint
+    {
+        Task<List<ProductModel>> GetAll();
+    }
+}
