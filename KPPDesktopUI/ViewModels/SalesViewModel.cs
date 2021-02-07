2ef5b2f5cac1680b0a2806c5d13f715378f82f51
smittyb <smittyb.sia@gmail.com>
2021-02-07 11:12:44 -0800

    Lesson 18 Completed and working.

--- KPPDesktopUI/ViewModels/SalesViewModel.cs
+++ KPPDesktopUI/ViewModels/SalesViewModel.cs
@@ -44,7 +44,34 @@ namespace KPPDesktopUI.ViewModels
             }
         }
 
-        private int _itemQuantity;
+        private ProductModel _selectedProduct;
+
+        public ProductModel SelectedProduct
+        {
+            get { return _selectedProduct; }
+            set
+            { 
+                _selectedProduct = value;
+                NotifyOfPropertyChange(() => SelectedProduct);
+                NotifyOfPropertyChange(() => CanAddToCart);
+            }
+        }
+
+        private BindingList<CartItemModel> _cart = new BindingList<CartItemModel>();
+
+        public BindingList<CartItemModel> Cart
+        {
+            get { return _cart; }
+            set
+            { 
+                _cart = value;
+                NotifyOfPropertyChange(() => Cart);
+            }
+        }
+
+
+
+        private int _itemQuantity = 1;
 
         public int ItemQuantity
         {
@@ -53,6 +80,7 @@ namespace KPPDesktopUI.ViewModels
             { 
                 _itemQuantity = value;
                 NotifyOfPropertyChange(() => ItemQuantity);
+                NotifyOfPropertyChange(() => CanAddToCart);
             }
         }
 
@@ -60,9 +88,14 @@ namespace KPPDesktopUI.ViewModels
         public string SubTotal
         {
             get
-            { 
-                // TODO - Replace with Calculation
-                return "$0.00"; 
+            {
+                decimal subTotal = 0;
+
+                foreach (var item in Cart)
+                {
+                    subTotal += (item.Product.RetailPrice * item.QuantityInCart);
+                }
+                return subTotal.ToString("C"); 
             }
         }
 
@@ -85,8 +118,6 @@ namespace KPPDesktopUI.ViewModels
         }
 
 
-
-
         public bool CanAddToCart
         {
             get
@@ -95,27 +126,39 @@ namespace KPPDesktopUI.ViewModels
 
                // Make sure something is selected
                // Make sure there is an item quantity
+               if (ItemQuantity > 0 && SelectedProduct?.QuantityInStock >= ItemQuantity)
+                {
+                    output = true;
+                }
 
                 return output;
             }
         }
 
-        private BindingList<ProductModel> _cart;
-
-        public BindingList<ProductModel> Cart
-        {
-            get { return _cart; }
-            set
-            {
-                _cart = value;
-                NotifyOfPropertyChange(() => Cart);
-            }
-        }
-
-
         public void AddToCart()
         {
+            CartItemModel existingItem = Cart.FirstOrDefault(x => x.Product == SelectedProduct);
 
+            if (existingItem != null)
+            {
+                existingItem.QuantityInCart += ItemQuantity;
+                // HACK -- There should be a better way of refreshing the display
+                Cart.Remove(existingItem);
+                Cart.Add(existingItem);
+            }
+            else
+            {
+                CartItemModel item = new CartItemModel
+                {
+                    Product = SelectedProduct,
+                    QuantityInCart = ItemQuantity
+                };
+                Cart.Add(item);
+            }
+
+            SelectedProduct.QuantityInStock -= ItemQuantity;
+            ItemQuantity = 1;
+            NotifyOfPropertyChange(() => SubTotal);
         }
 
         public bool CanRemoveFromCart
@@ -132,7 +175,7 @@ namespace KPPDesktopUI.ViewModels
 
         public void RemoveFromCart()
         {
-
+            NotifyOfPropertyChange(() => SubTotal);
         }
 
         public bool CanCheckOut
