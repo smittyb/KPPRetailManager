a783013edd0df6f9e7049aed343f725dfa56fe70
smittyb <smittyb.sia@gmail.com>
2021-02-07 22:59:08 -0800

    Update EF DataBase Name

--- KPPDataManager/Models/IdentityModels.cs
+++ KPPDataManager/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace KPPDataManager.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("EFData", throwIfV1Schema: false)
         {
         }
         
