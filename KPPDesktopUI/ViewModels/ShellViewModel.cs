1dfe569b5603cf8d9adaddff4dcf54e86821c626
smittyb <smittyb.sia@gmail.com>
2021-02-07 23:04:09 -0800

    Replace Container Reference in ShellVM
    with IoC

--- KPPDesktopUI/ViewModels/ShellViewModel.cs
+++ KPPDesktopUI/ViewModels/ShellViewModel.cs
@@ -13,18 +13,15 @@ namespace KPPDesktopUI.ViewModels
         private LoginViewModel _loginVM;
         private IEventAggregator _events;
         private SalesViewModel _salesVM;
-        private SimpleContainer _container;
 
-        public ShellViewModel(IEventAggregator events, SalesViewModel salesVM,
-            SimpleContainer container)
+        public ShellViewModel(IEventAggregator events, SalesViewModel salesVM)
         {
             _events = events;
             _salesVM = salesVM;
-            _container = container;
             
             _events.Subscribe(this);
 
-            ActivateItem(_container.GetInstance<LoginViewModel>());
+            ActivateItem(IoC.Get<LoginViewModel>());
         }
 
         public void Handle(LogOnEvent message)
